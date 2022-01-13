2455b503c05472749eff3d9e3748a12f07485e77
Behrouz Safari <behrouz.safari@gmail.com>
2022-01-13 20:59:48 +0100

    simplified

--- core.py
+++ core.py
@@ -6,49 +6,42 @@ from matplotlib.collections import LineCollection
 from hypatie.plots import plot_radec, plot_altaz
 from hypatie.transform import radec_to_altaz
 from hypatie.data import cities
-from stars import hip_stars, constellations
+from stardata import hip_stars, constellations
 from io import StringIO
-
-def constellations_func():
-    class cns:
-        def __init__(self, cns_str):
-            self.name = cns_str.split(' ')[0]
-            stars = cns_str.split(' ')[2:]
-            stars = [int(i) for i in stars]
-            self.edges = [tuple(stars[i:i+2]) for i in [*range(0,len(stars),2)]]
-
-    data = constellations.split('\n')[:-1]
-    dc = {}
-    for i in data:
-        tmp = cns(i)
-        dc[tmp.name] = tmp.edges
-
-    edges = []
-    for k,v in dc.items():
-        for i in v:
-            edges.append(i)
-
-    edge1 = [i[0] for i in edges]
-    edge2 = [i[1] for i in edges]
-
-    return edge1, edge2
 
 
 def draw_chart(obs_loc, t, alpha=0.3):
     lon, lat = obs_loc
 
+    # Read constellations data
+    # ------------------------
+    data = constellations.split('\n')[1:-1]
+    dc = {}
+    for i in data:
+        name = i.split(' ')[0]
+        stars = i.split(' ')[2:]
+        stars = [int(j) for j in stars]
+        edges = [tuple(stars[k:k+2]) for k in [*range(0,len(stars),2)]]
+        dc[name] = edges
+    edges = []
+    for k,v in dc.items():
+        for i in v:
+            edges.append(i)
+    edge1 = [i[0] for i in edges]
+    edge2 = [i[1] for i in edges]
+
+    # Read Hipparcos stars
+    # --------------------
     df = pd.read_csv(StringIO(hip_stars)).set_index('hip')
 
 
     marker_size = (0.5 + 7 - df['Vmag'].values) ** 2.0
 
-    t = t.isoformat()[:19].replace('T', ' ')
+    #t = t.isoformat()[:19].replace('T', ' ')
 
     alt, az = radec_to_altaz(
-        lon=lon,
-        lat=lat,
-        ra=df['ra'],
-        dec=df['dec'],
+        lon=lon, lat=lat,
+        ra=df['ra'], dec=df['dec'],
         t=t)
 
     new_df = pd.DataFrame(
@@ -76,5 +69,4 @@ def draw_chart(obs_loc, t, alpha=0.3):
 t = datetime(2022, 1, 13, 20, 21)
 obs_loc = cities['strasbourg'][:2]
 
-edge1, edge2 = constellations_func()
 draw_chart(obs_loc, t, alpha=0.4)
