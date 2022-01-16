453fb6ac87b62587f2f63d9676944a8d7baaa4f4
Behrouz <behrouz.safari@gmail.com>
2022-01-16 21:38:40 +0100

    add dt2jd and jd2dt

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 import numpy as np
 from collections.abc import Iterable
-from datetime import datetime
+from datetime import datetime, timedelta
 import math
 
 def radec_to_altaz(lon, lat, ra, dec, t):
@@ -41,33 +41,11 @@ def radec_to_altaz(lon, lat, ra, dec, t):
     alt = np.arcsin(zhor)*r2d
     return alt, az
 
-def date_to_jd(dt):
+def dt2jd(dt):
     return dt.toordinal() + 1721424.5
 
-# from here: https://gist.github.com/jiffyclub/1294443
-def jd_to_date(jd):
-    jd = jd + 0.5
-    F, I = math.modf(jd)
-    I = int(I)
-    A = math.trunc((I - 1867216.25)/36524.25)
-    if I > 2299160:
-        B = I + 1 + A - math.trunc(A / 4.)
-    else:
-        B = I
-    C = B + 1524
-    D = math.trunc((C - 122.1) / 365.25)
-    E = math.trunc(365.25 * D)
-    G = math.trunc((C - E) / 30.6001)
-    day = C - E + F - math.trunc(30.6001 * G)
-    
-    if G < 13.5:
-        month = G - 1
-    else:
-        month = G - 13
-        
-    if month > 2.5:
-        year = D - 4716
-    else:
-        year = D - 4715
-        
-    return year, month, day
+def jd2dt(jd):
+    jd0 = 2268923.5
+    t0 = datetime(1500,1,1)
+    dt =  timedelta(days=jd-jd0)
+    return t0+dt
