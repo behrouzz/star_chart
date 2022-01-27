0856f59828427625925fb4cea276be752f5bc7bb
Behrouz Safari <behrouz.safari@gmail.com>
2022-01-27 21:52:04 +0100

    nothing important

--- utils.py
+++ utils.py
@@ -3,17 +3,18 @@ import plotly.graph_objects as go
 def get_const_data(fig, edges, df):
     #cosnt_data = []
     for e in edges:
-        th1 = df.loc[e[0]]['az']
-        r1  = 90 - df.loc[e[0]]['alt']
-        th2 = df.loc[e[1]]['az']
-        r2  = 90 - df.loc[e[1]]['alt']
+        th1 = df.loc[e[1]]['az']
+        r1  = 90 - df.loc[e[1]]['alt']
+        th2 = df.loc[e[2]]['az']
+        r2  = 90 - df.loc[e[2]]['alt']
         tr = go.Scatterpolar(
             r=[r1,r2],
             theta=[th1,th2],
             mode='lines',
             line={'color':'gray'},
-            showlegend=False, #name='',
-            legendgroup='Costellations',
+            showlegend=False,
+            name=e[0],#
+            legendgroup=e[0],#
             hoverinfo='skip',#
             opacity=0.5)
         fig.add_trace(tr)
@@ -31,3 +32,10 @@ def get_star_data(fig, df_show, star_marker, star_hovertext):
         legendgroup='Stars')
     fig.add_trace(tr)
     return fig
+
+def create_edges(dc):
+    edges = []
+    for k,v in dc.items():
+        for i in v:
+            edges.append((k, i[0], i[1]))
+    return edges
