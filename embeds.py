ad54eb6af0db71423d9f62a7fd6c74746220369b
Behrouz Safari <behrouz.safari@gmail.com>
2022-01-31 18:35:30 +0100

    add slider

--- embeds.py
+++ embeds.py
@@ -9,12 +9,15 @@ def create_star_marker_hover(df_show):
                    'opacity':1,
                    'line':{'width':0}}
     # star_hover
+    #df_show.loc[df_show['temperature'].notnull(), 'temp'] = df_show[df_show['temperature'].notnull()].astype(int).astype(str)
+    df_show.loc[df_show['temperature'].isna(), 'temp'] = 'NA'
+    
     star_hovertext = '<b>'+df_show['name']+ '</b><br>' + \
                      '<i>'+df_show['long']+'</i><br>' + \
                      'RA: ' + df_show['ra'].apply(lambda x: round(x,5)).astype(str) + '<br>' + \
                      'DEC: ' + df_show['dec'].apply(lambda x: round(x,5)).astype(str) + '<br>' + \
                      'Vmag: ' + df_show['Vmag'].astype(str) + '<br>' + \
-                     'Temperature: ' + df_show['temperature'].astype(int).astype(str)
+                     'Temperature: ' + df_show['temp'].astype(str)
     
     return star_marker, star_hovertext
 
