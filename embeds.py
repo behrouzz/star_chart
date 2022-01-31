222d791ef8544188b51eaf4b66a8e342549fa3e5
Behrouz Safari <behrouz.safari@gmail.com>
2022-01-31 01:02:20 +0100

    time and location with dash

---
+++ embeds.py
@@ -0,0 +1,12 @@
+angularaxis = {'direction': "counterclockwise",
+               'rotation': 90,
+               'tickmode':'array',
+               'tickvals':[0,90,180,270],
+               'ticktext':['N','E','S','W'],
+               'gridcolor': '#222'}
+
+radialaxis = {'tickmode':'array',
+              'tickvals':[0,30,60,90],
+              'ticktext':['90','60','30','0'],
+              'gridcolor': '#222',
+              'linecolor': '#222'}
---
+++ locations.py
@@ -0,0 +1,51 @@
+locs = """
+country,city,lon,lat
+Afganistan,Kabul,69.1658,34.5328
+Australia,Sydney,151.2094,-33.865
+Brazil,Rio De Janeiro,-43.1964,-22.9083
+Brazil,Sao Paulo,-46.6339,-23.5504
+Brazil,Brasilia,-47.8828,-15.7939
+Canada,Toronto,-79.3733,43.7417
+Canada,Montreal,-73.5617,45.5089
+China,Beijing,116.3914,39.905
+China,Shanghai,121.4667,31.1667
+Egypt,Cairo,31.2394,30.0561
+France,Strasbourg,7.74408381754883,48.58313582900411
+France,Paris,2.3522,48.8566
+Germany,Munich,11.5755,48.1372
+Germany,Berlin,13.3833,52.5167
+Hong Kong,Hong Kong,114.185,22.305
+India,Dubai,55.3094,25.2697
+India,Delhi,77.23,28.66
+India,Mumbai,72.8333,18.9667
+Iran,Hamadan,48.5162,34.8065
+Iran,Qazvin,50.0041,36.2688
+Iran,Rasht,49.6,37.2833
+Iran,Mashhad,59.6042,36.3069
+Iran,Tehran,51.4167,35.7
+Iran,Esfahan,51.6675,32.6447
+Iran,Tabriz,46.2833,38.0833
+Iran,Shiraz,52.5425,29.61
+Iran,Karaj,50.9915,35.8327
+Iran,Ahvaz,48.6692,31.3203
+Italy,Rome,12.4828,41.8931
+Japon,Tokyo,139.6922,35.6897
+Mexico,Mexico City,-99.1333,19.4333
+Russia,Moscow,37.6178,55.7558
+South Africa,Johannesburg,28.0416,-26.2044
+South Korea,Seoul,126.99,37.56
+Spain,Valencia,-0.3764,39.47
+Spain,Barcelona,2.1769,41.3825
+Spain,Madrid,-3.7167,40.4167
+Thailand,Bangkok,100.5167,13.75
+Turkey,Istanbul,28.9603,41.01
+UK,London,-0.1275,51.5072
+UK,Manchester,-2.2453,53.4794
+USA,Los Angeles,-118.4068,34.1139
+USA,San Francisco,-122.443,37.7562
+USA,Chicago,-87.6862,41.8373
+USA,Boston,-71.0846,42.3188
+USA,Miami,-80.2102,25.7839
+USA,New York,-73.9249,40.6943
+USA,Washington,-77.0163,38.9047
+"""
