b0d277da644d70c1b94729473a914a5f59d6f2d4
Behrouz Safari <behrouz.safari@gmail.com>
2022-02-01 21:41:13 +0100

    add modul styles

--- embeds.py
+++ embeds.py
@@ -33,4 +33,21 @@ radialaxis = {'tickmode':'array',
               'gridcolor': '#222',
               'linecolor': '#222'}
 
+dd1_style = {'width':'250px',
+             'background-color':'black',
+             'color':'blue'}
 
+dd2_style = {'width':'250px',
+             'background-color':'black',
+             'color':'blue'}
+
+dt1_style = {#'width':'150px',
+             'height':'20px',
+             'background-color':'black',
+             'color':'white'}
+
+dt2_style = {'width':'115px',
+             'height':'20px',
+             'background-color':'black',
+             'color':'white'}
+#{'display':'inline-block'}
---
+++ styles.py
@@ -0,0 +1,19 @@
+dd1_style = {'width':'250px',
+             'background-color':'black',
+             'color':'blue'}
+#{'display':'inline-block'}
+
+dd2_style = {'width':'250px',
+             'background-color':'black',
+             'color':'blue'}
+
+dt1_style = {#'width':'150px',
+             'height':'20px',
+             'background-color':'black',
+             'color':'white'}
+
+dt2_style = {'width':'115px',
+             'height':'20px',
+             'background-color':'black',
+             'color':'white'}
+
